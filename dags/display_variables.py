e9d54cb6b593d955acfc873001a8fc06db5a5617
derinj7 <derin.j@astronomer.io>
2025-03-21 11:14:58 +0530

    Updated the airflow dag

--- dags/display_variables.py
+++ dags/display_variables.py
@@ -1,8 +1,6 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-import logging
 
 default_args = {
     'owner': 'airflow',
@@ -13,17 +11,9 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-def log_variables(**kwargs):
-    """
-    Simple function to log the task instance and context
-    """
-    logger = logging.getLogger(__name__)
-    logger.info("Task instance: %s", kwargs.get('task_instance'))
-    logger.info("Variables accessed via Jinja templating in the BashOperator task")
-    return "Variables accessed using Jinja templating"
 
 with DAG(
-    'display_env_variables',
+    'display_variables',
     default_args=default_args,
     description='DAG to display environment variables using Jinja templating',
     schedule_interval=None,
@@ -36,7 +26,7 @@ with DAG(
     display_task = BashOperator(
         task_id='display_variables',
         bash_command='''
-echo "=== Environment Variables from CI/CD using Jinja Templating ==="
+echo "=== Airflow Variables from CI/CD using Jinja Templating ==="
 echo "environment: {{ var.value.environment | default('Not found') }}"
 echo "api_key: {{ var.value.api_key[:4] }}*****{{ var.value.api_key[-4:] if var.value.api_key is defined and var.value.api_key|length > 8 else '****' }}"
 echo "database_url: {{ var.value.database_url[:4] }}*****{{ var.value.database_url[-4:] if var.value.database_url is defined and var.value.database_url|length > 8 else '****' }}"
@@ -46,12 +36,6 @@ echo "commit_sha: {{ var.value.commit_sha | default('Not found') }}"
         ''',
     )
     
-    # Log task completion
-    log_task = PythonOperator(
-        task_id='log_completion',
-        python_callable=log_variables,
-        provide_context=True,
-    )
     
     # Define task dependencies
-    display_task >> log_task
+    display_task
