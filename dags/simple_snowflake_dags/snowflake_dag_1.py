c53cc47d79b8a73b684f467181e1d9ecd0e16169
derinj7 <derin.j@astronomer.io>
2025-05-30 04:44:02 +0530

    Updated the schedule of snowflake_dag_1

--- dags/simple_snowflake_dags/snowflake_dag_1.py
+++ dags/simple_snowflake_dags/snowflake_dag_1.py
@@ -13,7 +13,7 @@ with DAG(
     "snowflake_dag_1",
     description="Fetch customer data from the sales_analytics database",
     start_date=datetime(2024, 1, 1),
-    schedule="0 18 * * *",  # Run daily at 6 PM
+    schedule="0 * * * *",  # Run every hour
     tags=["simple_snowflake_batch"],
     catchup=False,
 ) as dag:
@@ -21,6 +21,6 @@ with DAG(
     fetch_customers = SQLExecuteQueryOperator(
         task_id="fetch_customers",
         sql="SELECT * FROM CUSTOMERS;",
-        conn_id="snowflake_default",  # INTENTIONAL ERROR: Using incorrect connection ID to make test fail
+        conn_id="snowflake_default",  
         outlets=[customer_dataset],
     )
